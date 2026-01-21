c2e9af69de9c23cc94f946671722a93fc2130e46
jupedro2112-pixel <ju.pedro2112@gmail.com>
2026-01-21 21:18:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// index.js (un ejemplo simple de un servidor en Node.js)
+// index.js (servidor básico en Node.js con Express)
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
